349f485617249d105fb356a27801ccc70a7bf627
deepikasampangi <deepikasampangi@gmail.com>
2020-05-17 15:16:09 +0530

    send mail using script

--- asyncio/asyncio_basics.py
+++ asyncio/asyncio_basics.py
@@ -1,14 +1,29 @@
 import asyncio
+import random
 
 
 async def initial_coroutine():
+    process_time = random.randint(1, 10)
     print("Initial Co routine here")
 
 
-def main():
-    simple_loop = asyncio.get_event_loop()
-    simple_loop.run_until_complete(initial_coroutine())
-    simple_loop.close()
+# @asyncio.corotuine
+# def other_way_of_calling():
+#     print("Another way of calling a async coroutine, with annotions")
 
 
-main()
+async def random_int(id):
+    process_time = random.randint(1,10)
+    await asyncio.sleep(process_time)
+    print(f"Coroutine: {id}, has successfully completed after {process_time} seconds")
+
+async def main():
+    tasks = []
+    for i in range(10):
+        tasks.append(asyncio.ensure_future(random_int(i)))
+
+    await asyncio.gather(*tasks)
+
+simple_loop = asyncio.get_event_loop()
+simple_loop.run_until_complete(main())
+simple_loop.close()
